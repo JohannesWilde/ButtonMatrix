064b9888dd6e8209d30508ce62787e44640ec77a
Johannes Wilde <git@jowil.de>
2023-09-25 17:10:02 +0200

    Update ArduinoDrivers.

--- src/main.cpp
+++ src/main.cpp
@@ -16,27 +16,27 @@ int main()
 {
     typedef ArduinoUno arduinoUno;
 
-    typedef arduinoUno::C5 VccPeriphery;
-    VccPeriphery::setType(AvrInputOutput::OUTPUT_HIGH);
+    typedef arduinoUno::pinC5 VccPeriphery;
+    VccPeriphery::setType(AvrInputOutput::OutputHigh);
 
-    typedef RsLatch<DummyAvrPin1, arduinoUno::C1, DummyAvrPin1> rsLatch;
+    typedef RsLatch<DummyAvrPin1, arduinoUno::pinC1, DummyAvrPin1> rsLatch;
     rsLatch::initialize();
 
     typedef ParallelInShiftRegister74HC165<8,
-            arduinoUno::C3,
-            arduinoUno::C2,
+            arduinoUno::pinC3,
+            arduinoUno::pinC2,
             DummyAvrPin1,
-            arduinoUno::C4,
+            arduinoUno::pinC4,
             DummyAvrPin1,
             DummyAvrPin1> parallelInShiftRegister;
     parallelInShiftRegister::initialize();
 
     typedef ShiftRegister74HC595<8,
-            arduinoUno::D2,
-            arduinoUno::D5,
-            arduinoUno::D4,
-            arduinoUno::D3,
-            arduinoUno::D6> shiftRegister;
+            arduinoUno::pinD2,
+            arduinoUno::pinD5,
+            arduinoUno::pinD4,
+            arduinoUno::pinD3,
+            arduinoUno::pinD6> shiftRegister;
     shiftRegister::initialize();
 
 
