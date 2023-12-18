0f947f70284838cc2cae7161b85b2fa3ebe0be15
Johannes Wilde <git@jowil.de>
2023-12-18 01:26:59 +0100

    Add further levels.

--- src/main.cpp
+++ src/main.cpp
@@ -210,6 +210,22 @@ uint8_t constexpr levels[][4] = {
     {createBitmaskToPress<0>(), createBitmaskToPress<1, 12>(), createBitmaskToPress<2, 17, 23>(), createBitmaskToPress<3>()},
     {createBitmaskToPress<0>(), createBitmaskToPress<1, 12, 13>(), createBitmaskToPress<2, 17>(), createBitmaskToPress<3>()},
     {createBitmaskToPress<0, 3, 7>(), createBitmaskToPress<1, 11>(), createBitmaskToPress<2>(), createBitmaskToPress<3>()}, // 0x10
+    {createBitmaskToPress<0, 1>(), createBitmaskToPress<1>(), createBitmaskToPress<2, 16, 21>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 3>(), createBitmaskToPress<1, 12>(), createBitmaskToPress<2, 19>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 8, 9, 14>(), createBitmaskToPress<2>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 9, 13>(), createBitmaskToPress<2, 21>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 11, 15>(), createBitmaskToPress<2>(), createBitmaskToPress<3, 24>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 10, 12>(), createBitmaskToPress<2, 18>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 2>(), createBitmaskToPress<1, 12>(), createBitmaskToPress<2, 21>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 7>(), createBitmaskToPress<1, 11, 15>(), createBitmaskToPress<2>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 6>(), createBitmaskToPress<1, 8>(), createBitmaskToPress<2, 16>(), createBitmaskToPress<3, 24>()},
+    {createBitmaskToPress<0, 0>(), createBitmaskToPress<1, 14>(), createBitmaskToPress<2, 16>(), createBitmaskToPress<3, 24>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 8, 15>(), createBitmaskToPress<2, 22, 23>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 0, 3>(), createBitmaskToPress<1, 13>(), createBitmaskToPress<2, 20>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 8, 12>(), createBitmaskToPress<2, 18, 23>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 2, 6>(), createBitmaskToPress<1, 12>(), createBitmaskToPress<2, 20>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0>(), createBitmaskToPress<1, 9, 12, 14>(), createBitmaskToPress<2, 22>(), createBitmaskToPress<3>()},
+    {createBitmaskToPress<0, 6, 7>(), createBitmaskToPress<1>(), createBitmaskToPress<2, 17, 19>(), createBitmaskToPress<3>()}, // 0x20
     // {createBitmaskToPress<0>(), createBitmaskToPress<1>(), createBitmaskToPress<2>(), createBitmaskToPress<3>()},
     // {createBitmaskToPress<0>(), createBitmaskToPress<1>(), createBitmaskToPress<2>(), createBitmaskToPress<3>()},
 
